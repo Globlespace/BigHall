d7f78dead245b6d1d2675d719009c3ebd3b6d2ff
syed huzaif <syedhuzaif120@gmail.com>
2022-04-05 14:55:01 +0530

    add cat

--- app/View/User/Category/Category.php
+++ app/View/User/Category/Category.php
@@ -1,6 +1,8 @@
 <@Page>
 <link href="<?= STYLE?>Category.css" type="text/css"/>
 <main>
-
+    <div class="head-line">
+        <h2 >Top deals from Oppo, realme & Samsung</h2>
+    </div>
 </main>
 <@Page>
