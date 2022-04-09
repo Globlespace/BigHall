486591ee843ea2303472c50a3101a9ac131d46c7
huzaif <huzaif@gmail.com>
2022-04-09 15:33:48 +0530

    category change

--- app/View/User/Category/Category.php
+++ app/View/User/Category/Category.php
@@ -1,8 +1,43 @@
 <@Page>
-<link href="<?= STYLE?>Category.css" rel="stylesheet"/>
+<link href="<?= STYLE?>Category.css" rel="stylesheet" />
 <main>
-    <div class="head-line">
-        <h2 >Top deals from Oppo, realme & Samsung</h2>
+    <div class="main-container">
+        <div class="con-heading-one">
+            <h1>Top Rated</h1>
+            <a>See More </a>
+        </div>
+        <div class="product">
+                <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+
+                </div>
+            </div>
+            <div class="product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="far fa-thin fa-star checked"></i>
+                    <i class="far fa-thin fa-star checked"></i>
+                    <i class="far fa-thin fa-star checked"></i>
+                    <i class="far fa-thin fa-star checked"></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
     </div>
 </main>
 <@Page>
