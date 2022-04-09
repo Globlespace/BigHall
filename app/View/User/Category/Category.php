76dda190f99460dce04e119d0a1a5aca82c3d4bc
huzaif <huzaif@gmail.com>
2022-04-09 17:03:23 +0530

    category complete

--- app/View/User/Category/Category.php
+++ app/View/User/Category/Category.php
@@ -1,10 +1,42 @@
 <@Page>
 <link href="<?= STYLE?>Category.css" rel="stylesheet" />
 <main>
+    <div class="con-heading-one">
+        <h1>Categories</h1>
+    </div>
     <div class="main-container">
-        <div class="con-heading-one">
-            <h1>Top Rated</h1>
-            <a>See More </a>
+        <div class="product">
+                <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
         </div>
         <div class="product">
                 <div class="product-img">
@@ -16,20 +48,21 @@
                     <spam class="price-whole">1900</spam>
                 </div>
                 <div class="strike-price">
-
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
                 </div>
             </div>
-            <div class="product-name">
+            <div class="cat-product-name">
                 <span class="assign-title">
                     Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
                 </span>
             </div>
             <div class="product-stars">
                 <div class="stars">
-                    <i class="far fa-thin fa-star checked"></i>
-                    <i class="far fa-thin fa-star checked"></i>
-                    <i class="far fa-thin fa-star checked"></i>
-                    <i class="far fa-thin fa-star checked"></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
                     <i class="far fa-thin fa-star "></i>
                     <i class="far fa-thin fa-star "></i>
                 </div>
@@ -38,6 +71,271 @@
                 </spam>
             </div>
         </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-img">
+                <img src="<?= IMAGES ?>img-1.jpg" alt="product image" >
+            </div>
+            <div class="product-rate">
+                <div class="assign-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">1900</spam>
+                </div>
+                <div class="strike-price">
+                    <spam class="price-symbol">₹</spam>
+                    <spam class="price-whole">600</spam>
+                </div>
+            </div>
+            <div class="cat-product-name">
+                <span class="assign-title">
+                    Fabtec Waterproof Car Body Cover with Mirror and Antenna Pocket and Soft Cotton Lining (Full Bottom Elastic Triple Stitched) (Metallic Silver) (for Maruti Baleno)
+                </span>
+            </div>
+            <div class="product-stars">
+                <div class="stars">
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="fas fa-solid fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                    <i class="far fa-thin fa-star "></i>
+                </div>
+                <spam class="star-number">
+                    2,637
+                </spam>
+            </div>
+        </div>
+
     </div>
 </main>
 <@Page>
